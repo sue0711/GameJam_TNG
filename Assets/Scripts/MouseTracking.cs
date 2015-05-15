726eb843b79a6f377b7d985e520b8768e0ae8ece
sue0711 <godqlswlq@gmail.com>
2015-05-16 04:49:09 +0900

    합치는듕

--- Assets/Scripts/MouseTracking.cs
+++ Assets/Scripts/MouseTracking.cs
@@ -5,24 +5,81 @@ public class MouseTracking : MonoBehaviour {
 
     float Distance = 10;
 
-    void OnMouseDrag()
-    {
-        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    bool isFree;
 
-        transform.position = objPosition;
-    }
+    int freeCnt;
 
-	
-    /*    
-        // Use this for initialization
+    PointEffector2D blackHolePE;
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
 	void Start () {
-	
+        isFree = true;
+        freeCnt = 0;
+        blackHolePE = GameObject.Find("Hole").GetComponent<PointEffector2D>();
+        rb = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
+        if (freeCnt >= 3 && !isFree)
+        {
+            blackHolePE.forceMagnitude = 500;
+        }
+        
+        /*
+        if (isFree)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+                transform.position = objPosition;
+            }
+        }
+        */
+        if (!isFree)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                freeCnt++;
+                //blackHolePE.forceMagnitude += 50;
+            }
+
+            float distance = Vector2.Distance(blackHolePE.gameObject.transform.position, this.transform.position);
+            Debug.Log(distance);
+
+            if (distance > 12.0f)
+            {
+                isFree = true;
+                rb.isKinematic = true;
+                rb.isKinematic = false;
+                blackHolePE.forceMagnitude = -50;
+                freeCnt = 0;
+            }
+        }
 	}
-     */
+   
+    void OnMouseDrag()
+    {
+        if (isFree)
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            transform.position = objPosition;
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BlackHole")
+        {
+            isFree = false;
+        }
+    }
 }
