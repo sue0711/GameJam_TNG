2e1a0400b3a8ccf73371f829df01a6b1bb060ec9
choeunyee <choeunyee@gmail.com>
2015-05-16 02:06:20 +0900

    TAIL ㅠㅠ

---
+++ Assets/Scripts/Tail.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tail : MonoBehaviour {
+
+
+    public GameObject BeforeObj;
+    public float distance = 2.0f;
+    
+
+	void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+        this.transform.position = (this.transform.position 
+                            - BeforeObj.transform.position).normalized * distance + BeforeObj.transform.position;
+    }
+
+
+   
+}
