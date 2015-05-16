1d53110282172bed6904ea2a0dfc122aa1730e2e
sue0711 <godqlswlq@gmail.com>
2015-05-16 09:55:59 +0900

    d
    d

---
+++ Assets/Scripts/Player_Collide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Collide : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Obstacle")
+        {
+            Debug.Log(this.gameObject.name + "가 " + coll.gameObject.tag + "랑 충돌함");
+        }
+    }
+}
---
+++ Assets/testTR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testTR : MonoBehaviour {
+
+    public float ScrollSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0, -1 * ScrollSpeed, 0);
+        if (this.transform.position.y <= -12.0f)
+        {
+            this.transform.Translate(0, 24, 0);
+        }
+	}
+}
