f517120387ee6bda9b1fad5b1366893bb95ac488
sue0711 <godqlswlq@gmail.com>
2015-05-16 11:17:20 +0900

    d
    d

---
+++ Assets/Scripts/StartUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartUI : MonoBehaviour {
+
+    public void StartBtnClicked()
+    {
+        Application.LoadLevel("Game_Scene");
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
